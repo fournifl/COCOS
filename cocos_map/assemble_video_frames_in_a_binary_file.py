102163412b6eac6b268dc36b655fe3eb50199da2
Florent Fournier <florent.fournier@protonmail.com>
2022-03-23 12:28:50 +0100

    save video frames with savez_compressed

--- cocos_map/assemble_video_frames_in_a_binary_file.py
+++ cocos_map/assemble_video_frames_in_a_binary_file.py
@@ -11,6 +11,7 @@ import pickle
 from glob import glob
 import numpy as np
 import pickle as pk
+import matplotlib.pyplot as plt
 from pathlib import Path
 
 
@@ -28,7 +29,9 @@ dir_frames = data_dir.joinpath('frames_projected/')
 ls = sorted(dir_frames.rglob("P*.png"))
 
 # number of used frames
-n = 25
+# n = 200
+# n = 400
+n = None
 if n is not None:
     ls = ls[0:n]
 else:
@@ -44,6 +47,7 @@ dt = 0.5
 # grid projected coordinates
 f_coords = data_dir.joinpath('coords_projected_grid.pkl')
 grid_coords = pickle.load(open(f_coords, 'rb'), encoding='latin-1')
+
 # grid_coords = pickle.load(open('/home/florent/dev/COCOS/data/raw/palavas/cristal_1/coords_projected_grid.pkl', 'rb'), encoding='latin-1')
 dx = round(grid_coords['utmx_projected_grid'][0, 1] - grid_coords['utmx_projected_grid'][0, 0], 2)
 
@@ -52,6 +56,8 @@ georef = json.load(open('/home/florent/ownCloud/Projets/SuiviVideo/Palavas/camer
 
 # update grid projection coordinates with camera position
 camera_position =  georef['Grid_Coordinate_System_Offset']
+grid_coords['utmx_projected_grid'] = grid_coords['utmx_projected_grid'].astype(float)
+grid_coords['utmy_projected_grid'] = grid_coords['utmy_projected_grid'].astype(float)
 grid_coords['utmx_projected_grid'] += camera_position[0]
 grid_coords['utmy_projected_grid'] += camera_position[1]
 
@@ -62,16 +68,21 @@ output_dict['Y'] = grid_coords['utmy_projected_grid']
 output_dict['dx'] = dx
 output_dict['dt'] = dt
 
-output_dict['RectMov_gray'] = np.zeros((height, width, n))
+output_dict['RectMov_gray'] = np.zeros((height, width, n)) * np.nan
 
 for i, f in enumerate(ls):
     # conversion of frame to gray
     img = cv2.imread(str(f))
     img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     output_dict['RectMov_gray'][:, :, i] = img_gray
+    # plt.imshow(img_gray)
+    # plt.show()
+    # pdb.set_trace()
 
 # save output dictionnary
-pickle.dump(output_dict, open(data_dir.joinpath('Video_' + sitename + '.pkl'), 'wb'))
-# np.savez(data_dir.joinpath('Video_' + sitename), output_dict)
+# pickle.dump(output_dict, open(data_dir.joinpath('Video_' + sitename + '.pkl'), 'wb'))
+np.savez_compressed(data_dir.joinpath('Video_compressed_' + sitename), RectMov_gray=output_dict['RectMov_gray'],
+                    X=output_dict['X'], Y=output_dict['Y'], dx=output_dict['dx'], dt=output_dict['dt'])
+# np.savez(data_dir.joinpath('Video_' + sitename), RectMov_gray=output_dict['RectMov_gray'],
+#                     X=output_dict['X'], Y=output_dict['Y'], dx=output_dict['dx'], dt=output_dict['dt'])
 
-    
