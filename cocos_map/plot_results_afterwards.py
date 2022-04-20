f9fb3c8fffbdf3fec2f05835c5842647112869e5
Florent Fournier <florent.fournier@protonmail.com>
2022-04-20 16:25:54 +0200

    more x,y min max params for Palavas

--- cocos_map/plot_results_afterwards.py
+++ cocos_map/plot_results_afterwards.py
@@ -22,6 +22,7 @@ def plot_bathy(results, output_dir_plot, basename, depth_lims, diff_depth_lims, 
             mask = np.isnan(results['Dk'][k])
             diff_depth_ground_truth = results['Dk'][k] - results['Dgt']
             im1 = ax[0].pcolor(results['grid_X'], results['grid_Y'], results['Dk'][k], cmap='jet_r')
+            pdb.set_trace()
             im2 = ax[1].pcolor(results['grid_X'], results['grid_Y'], np.ma.array(results['Dgt'], mask=mask), cmap='jet_r')
             im3 = ax[2].pcolor(results['grid_X'], results['grid_Y'], diff_depth_ground_truth, cmap='Spectral')
 
@@ -212,77 +213,87 @@ def plot_all_diags(results, output_dir_plot, basename, depth_lims, diff_depth_li
 # execution options
 plot_only_bathy = True
 plot_all_results = False
+date = '20220314'
+hour = '08h'
 
 # configuration corresponding to given results
-# fieldsite = 'wavecams_palavas_cristal'
-# cam_name = 'cristal_3'
+fieldsite = 'wavecams_palavas_cristal'
+cam_name = 'cristal_3'
 # fieldsite = 'wavecams_palavas_cristal_merged'
 # fieldsite = 'wavecams_palavas_stpierre'
 # cam_name = 'st_pierre_3'
-fieldsite = 'wavecams_palavas_cristal_merged'
-cam_name = 'cristal_merged'
-cpu_speed = 'exact' #'fast','normal','slow', 'accurate', 'exact'
+# fieldsite = 'wavecams_palavas_cristal_merged'
+# cam_name = 'cristal_merged'
+cpu_speeds = ['slow', 'accurate'] #'fast','normal','slow', 'accurate', 'exact'
 calcdmd = 'standard' # standard or robust
 
-# load results
-output_dir = f'/home/florent/dev/COCOS/results/{fieldsite}/{cam_name}/'
-f_results = glob(output_dir + f'/results_CPU_speed_{cpu_speed}_calcdmd_{calcdmd}_exec_time_*.npz')[0]
-results = np.load(f_results)
-basename = Path(f_results).stem
+for cpu_speed in cpu_speeds:
+    # load results
+    output_dir = f'/home/florent/dev/COCOS/results/{fieldsite}/{cam_name}/{date}/{hour}/'
+    f_results = glob(output_dir + f'/results_CPU_speed_{cpu_speed}_calcdmd_{calcdmd}_exec_time_*.npz')[0]
+    results = np.load(f_results)
+    basename = Path(f_results).stem
 
-# plot options
-print(f'xmin = {int(results["grid_X"].min())}')
-print(f'xmax = {int(results["grid_X"].max())}')
-print(f'ymin = {int(results["grid_Y"].min())}')
-print(f'ymax = {int(results["grid_Y"].max())}')
-if cam_name == 'st_pierre_2':
-    xmin = 574300
-    xmax = 574700
-    ymin = 4818950
-    ymax = 4819370
-elif cam_name == 'st_pierre_1':
-    xmin = 574650
-    xmax = 575112
-    ymin = 4819178
-    ymax = 4819514
-elif cam_name == 'st_pierre_3':
-    xmin = 574500
-    xmax = 574925
-    ymin = 4819088
-    ymax = 4819383
-if cam_name == 'cristal_merged':
-    xmin = -488
-    xmax = 487
-    ymin = 12
-    ymax = 487
-emprise_plot = [xmin, xmax, ymin, ymax]
+    # plot options
+    print(f'xmin = {int(results["grid_X"].min())}')
+    print(f'xmax = {int(results["grid_X"].max())}')
+    print(f'ymin = {int(results["grid_Y"].min())}')
+    print(f'ymax = {int(results["grid_Y"].max())}')
+    if cam_name == 'cristal_1':
+        xmin = 575800
+        xmax = 576140
+        ymin = 4819730
+        ymax = 4820080
+    if cam_name == 'cristal_2':
+        xmin = 575600
+        xmax = 576100
+        ymin = 4819600
+        ymax = 4820100
+    if cam_name == 'cristal_3':
+        xmin = 575910
+        xmax = 576430
+        ymin = 4819750
+        ymax = 4820200
 
-# check if ground truth exists:
-# ground_truth_exists = type(results['Dgt']).__module__ == np.__name__
-ground_truth_exists = False
+    elif cam_name == 'st_pierre_2':
+        xmin = 574300
+        xmax = 574700
+        ymin = 4818950
+        ymax = 4819370
+    elif cam_name == 'st_pierre_1':
+        xmin = 574650
+        xmax = 575112
+        ymin = 4819178
+        ymax = 4819514
+    elif cam_name == 'st_pierre_3':
+        xmin = 574500
+        xmax = 574925
+        ymin = 4819088
+        ymax = 4819383
+    elif cam_name == 'cristal_merged':
+        xmin = -488
+        xmax = 487
+        ymin = 12
+        ymax = 487
+    emprise_plot = [xmin, xmax, ymin, ymax]
 
-# affichage
-depth_lims = [0, 8]
-diff_depth_lims = [-1.5, 1.5]
+    # check if ground truth exists:
+    ground_truth_exists = type(results['Dgt']).__module__ == np.__name__
+    # ground_truth_exists = False
 
-if plot_only_bathy:
-    dir_plot = 'plots_only_bathy'
-    output_dir_plot = f'/home/florent/dev/COCOS/results/{fieldsite}/{cam_name}/{dir_plot}/'
-    Path(output_dir_plot).mkdir(parents=True, exist_ok=True)
-    plot_bathy(results, output_dir_plot, basename, depth_lims, diff_depth_lims, emprise_plot, ground_truth_exists=ground_truth_exists)
-if plot_all_results * ground_truth_exists:
-    kalman_error_lims = [0, 1]
-    freqlims = [1 / 3, 1 / 15]
-    dir_plot = 'plots_all_results'
-    output_dir_plot = f'/home/florent/dev/COCOS/results/{fieldsite}/{cam_name}/{dir_plot}/'
-    Path(output_dir_plot).mkdir(parents=True, exist_ok=True)
-    plot_all_diags(results, output_dir_plot, basename, depth_lims, diff_depth_lims, kalman_error_lims, freqlims)
+    # affichage
+    depth_lims = [0, 8]
+    diff_depth_lims = [-1.5, 1.5]
 
-
-
-
-
-
-
-
-
+    if plot_only_bathy:
+        dir_plot = 'plots_only_bathy'
+        output_dir_plot = f'{output_dir}/{dir_plot}/'
+        Path(output_dir_plot).mkdir(parents=True, exist_ok=True)
+        plot_bathy(results, output_dir_plot, basename, depth_lims, diff_depth_lims, emprise_plot, ground_truth_exists=ground_truth_exists)
+    if plot_all_results * ground_truth_exists:
+        kalman_error_lims = [0, 1]
+        freqlims = [1 / 3, 1 / 15]
+        dir_plot = 'plots_all_results'
+        output_dir_plot = f'/home/florent/dev/COCOS/results/{fieldsite}/{cam_name}/{dir_plot}/'
+        Path(output_dir_plot).mkdir(parents=True, exist_ok=True)
+        plot_all_diags(results, output_dir_plot, basename, depth_lims, diff_depth_lims, kalman_error_lims, freqlims)
