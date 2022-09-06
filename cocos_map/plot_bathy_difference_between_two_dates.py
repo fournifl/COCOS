133583bc1370c75817b37ac67a974cc6868749cb
Florent Fournier <florent.fournier@protonmail.com>
2022-09-06 18:49:29 +0200

    minor changes in function plot_gathered_diff_results

--- cocos_map/plot_bathy_difference_between_two_dates.py
+++ cocos_map/plot_bathy_difference_between_two_dates.py
@@ -68,16 +68,11 @@ def interpolate_results_on_same_grid(results, X, Y, k, cam_names_ordered):
     return results_Dk_common_grid_gathered, results_Dk_common_grid
 
 
-def interpolate_lidar_data_on_gathered_grid(lidar_data, X, Y, mask):
-    Z = griddata((np.ravel(litto3d['Xi']), np.ravel(litto3d['Yi'])), np.ravel(litto3d['zi']), (X, Y), method='linear')
-    Z[mask] = np.nan
-    return Z
 
-
-def plot_gathered_diff_results(cpu_speed, X, Y, results_Dk_gathered, results_Dk_common_grid, depth_lims, diff_depth_lims,
+def plot_gathered_diff_results(cpu_speed, X, Y, results_Dk_common_grid, depth_lims, diff_depth_lims,
                           output_dir_plot, emprise, k, cam_name_central, vertical_ref, WL_ref_IGN69, resolutions,
                           resolution, key_dates):
-    fig, ax = plt.subplots(2, 2, figsize=(15, 15), frameon=False)
+    fig, ax = plt.subplots(2, 2, figsize=(25, 15), frameon=False)
 
     # make sure to order cam_names so as to have central camera plotted first
     cam_names = list(results_Dk_common_grid[key_dates[0]].keys())
@@ -124,45 +119,32 @@ def plot_gathered_diff_results(cpu_speed, X, Y, results_Dk_gathered, results_Dk_
     ax[0, 1].set_title(f'diff depth [m], {key_dates[1]} vs {key_dates[0]}')
     ax[0, 1].set_xlim([emprise[0], emprise[1]])
     ax[0, 1].set_ylim([emprise[2], emprise[3]])
-    plt.show()
+    dates = f'{key_dates[1]}_vs_{key_dates[0]}'
+    fig.delaxes(ax[1, 1])
+    plt.tight_layout()
+    fig.savefig(Path(output_dir_plot).joinpath(f'diff_bathy_{dates}_cpu_speed_{cpu_speed}_k_{k}_res_{resolution}' + '.jpg'))
 
-
-    fig.colorbar(im0, ax=ax[0])
-    # fig.colorbar(im1, ax=ax[1])
-    #
-    # ax[0].axis('equal')
-    # ax[0].set_title('depth for each camera [m]')
-    # ax[0].set_ylabel('y [m]')
-    # ax[0].set_xlabel('x [m]')
-    # ax[0].set_xlim([emprise[0], emprise[1]])
-    # ax[0].set_ylim([emprise[2], emprise[3]])
-    # ax[1].set_title('depth gathered [m]')
-    # ax[1].axis('equal')
-    # ax[1].set_title('depth gathered [m]')
-    # ax[1].set_ylabel('y [m]')
-    # ax[1].set_xlabel('x [m]')
-    # ax[1].set_xlim([emprise[0], emprise[1]])
-    # ax[1].set_ylim([emprise[2], emprise[3]])
-    #
-    # fig.savefig(Path(output_dir_plot).joinpath(
-    #     f'results_gathered_k_{k}_cpu_speed_{cpu_speed}_vertical_ref_{vertical_ref}_res_{resolution}' + '.jpg'))
-    # plt.close('all')
 
 
 # execution option
 k = -1
 
 vertical_ref = 'IGN69'#'WL' or 'IGN69'
-fieldsite = 'wavecams_palavas_cristal'
-cam_names = ['cristal_1', 'cristal_2', 'cristal_3']
-cam_name_central = 'cristal_1'
+# fieldsite = 'wavecams_palavas_cristal'
+# cam_names = ['cristal_1', 'cristal_2', 'cristal_3']
+# cam_name_central = 'cristal_1'
+fieldsite = 'wavecams_palavas_stpierre'
+cam_name_central = 'st_pierre_3'
+cam_names = ['st_pierre_1', 'st_pierre_2', 'st_pierre_3']
 # cpu_speeds = ['fast', 'normal', 'slow', 'accurate', 'exact'] #'fast','normal','slow', 'accurate', 'exact'
 cpu_speeds = ['accurate']
 calcdmd = 'standard' # standard or robust
 
 # dates and hours of respective computed bathymetries
-dates = ['20220314', '20220314']
-hours = ['07h', '08h']
+# dates = ['20220314', '20220314']
+# hours = ['07h', '08h']
+dates = ['20220314', '20220323']
+hours = ['08h', '15h']
 results = {}
 
 # cam_names_ordered so as to have cam_central in last position
@@ -207,71 +189,10 @@ for cpu_speed in cpu_speeds:
     emprise_plot = [xmin, xmax, ymin, ymax]
 
     depth_lims = [0, 6]
-    diff_depth_lims = [-0.5, 0.5]
+    diff_depth_lims = [-1.0, 1.0]
 
-    output_dir_plot = f'/home/florent/dev/COCOS/results/{fieldsite}/gathered/comparison_between_2_dates/' \
-                      f'{key_dates[0]}_vs_{key_dates[1]}/'
+    output_dir_plot = f'/home/florent/dev/COCOS/results/{fieldsite}/gathered/comparison_between_2_dates/'
     Path(output_dir_plot).mkdir(parents=True, exist_ok=True)
-    plot_gathered_diff_results(cpu_speed, X, Y, results_Dk_common_grid_gathered, results_Dk_common_grid, depth_lims,
-                               diff_depth_lims, output_dir_plot, emprise_plot, k, cam_name_central, vertical_ref,
-                               WL_ref_IGN69, resolutions, resolution, key_dates)
-
-
-# for cpu_speed in cpu_speeds:
-#     print(f'cpu_speed: {cpu_speed}')
-#     # load results
-#     results = {}
-#     for cam_name in cam_names:
-#         output_dir = f'/home/florent/dev/COCOS/results/{fieldsite}/{cam_name}/{date}/{hour}/'
-#         try:
-#             f_results = glob(output_dir + f'/results_CPU_speed_{cpu_speed}_calcdmd_{calcdmd}_exec_time_*.npz')[0]
-#             results[cam_name] = np.load(f_results)
-#             results_exist = True
-#         except IndexError:
-#             results_exist = False
-#             continue
-#     if not results_exist:
-#         print(f'no result for cpu_speed {cpu_speed}')
-#         continue
-#     basename = Path(f_results).stem
-#
-#     # merge results
-#     X, Y, resolution, resolutions = compute_gathered_grid(results)
-#     # cam_names_ordered so as to have cam_central in last position
-#     cam_names_ordered = copy(cam_names)
-#     cam_names_ordered.append(cam_names_ordered.pop(cam_names_ordered.index(cam_name_central)))
-#     # merging
-#     results_Dk_common_grid_gathered, results_Dk_common_grid = interpolate_results_on_same_grid(results, X, Y, k, cam_names_ordered)
-#
-#     # check if ground truth exists:
-#     ground_truth_exists = False
-#     # ground_truth_exists = type(results[cam_names[-1]]['Dgt']).__module__ == np.__name__
-#
-#     # define WL_ref_IGN69
-#     if date == '20220314':
-#         WL_ref_IGN69 = 0.60 - 0.307
-#     elif date == '20220323':
-#         WL_ref_IGN69 = 0.19 - 0.307
-#
-#     # interpolate lidar data on gathered grid
-#     mask = np.isnan(results_Dk_common_grid_gathered)
-#     if ground_truth_exists:
-#         if  fieldsite == 'wavecams_palavas_cristal':
-#             f_litto3d = '/home/florent/Projects/Palavas-les-flots/Bathy/litto3d/cristal/litto3d_Palavas_epsg_32631_775_776_6271.pk'
-#         elif fieldsite == 'wavecams_palavas_stpierre':
-#             f_litto3d = '/home/florent/Projects/Palavas-les-flots/Bathy/litto3d/st_pierre/litto3d_Palavas_st_pierre_epsg_32631_774_775_6270.pk'
-#         litto3d = pickle.load(open(f_litto3d, 'rb'))
-#         z_lidar = interpolate_lidar_data_on_gathered_grid(litto3d, X, Y, mask)
-#
-#         # convert lidar topo to depth and change vertical ref if necessary
-#         if vertical_ref == 'WL':
-#             z_lidar = - z_lidar + WL_ref_IGN69
-#         elif vertical_ref == 'IGN69':
-#             z_lidar = - z_lidar
-#
-#     # plot bathy results only
-#     output_dir_plot = f'/home/florent/dev/COCOS/results/{fieldsite}/gathered/bathy_results_only/{date}/{hour}/'
-#     Path(output_dir_plot).mkdir(parents=True, exist_ok=True)
-#     plot_gathered_results(cpu_speed, X, Y, results_Dk_common_grid_gathered, results_Dk_common_grid, depth_lims,
-#                           output_dir_plot, emprise_plot, k, cam_name_central, vertical_ref, WL_ref_IGN69, resolutions,
-#                           resolution)
+    plot_gathered_diff_results(cpu_speed, X, Y, results_Dk_common_grid, depth_lims, diff_depth_lims, output_dir_plot,
+                               emprise_plot, k, cam_name_central, vertical_ref, WL_ref_IGN69, resolutions, resolution,
+                               key_dates)
