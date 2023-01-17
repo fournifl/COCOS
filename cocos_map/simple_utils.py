ef5edc2895654bdf05659077f8bbdab6834b48d4
Florent Fournier <florent.fournier@protonmail.com>
2023-01-17 19:16:19 +0100

    cleaning of imports

--- cocos_map/simple_utils.py
+++ cocos_map/simple_utils.py
@@ -1,12 +1,10 @@
 """
 @author: fournifl
 """
-import pdb
+
 
 import numpy as np
-from PIL import Image
 import cv2
-import time
 
 def optional_print(to_print, end='\n'):
     bool_print = False
@@ -23,9 +21,6 @@ def assemble_frames_paths_into_3d_array(ls, start, stop, m, n, n_frames_in_video
     for i in range(start, stop):
         frame = cv2.imread(str(ls[i]), cv2.IMREAD_GRAYSCALE)
         frames_array[:, :, i] = frame
-    # for i, frame_img in enumerate(ls):
-    #     frame = cv2.imread(str(frame_img), cv2.IMREAD_GRAYSCALE)
-    #     frames_array[:, :, i] = frame
     return frames_array, l
 
 
