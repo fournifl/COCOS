0d663813db36463ae81ee5f62019e79c043bc364
Florent Fournier <florent.fournier@protonmail.com>
2023-01-17 19:14:21 +0100

    creation of function assemble_frames_paths_into_3d_array

--- cocos_map/simple_utils.py
+++ cocos_map/simple_utils.py
@@ -1,11 +1,32 @@
 """
 @author: fournifl
 """
+import pdb
 
+import numpy as np
+from PIL import Image
+import cv2
+import time
 
 def optional_print(to_print, end='\n'):
     bool_print = False
     if bool_print:
         print(to_print, end=end)
+
+
+def assemble_frames_paths_into_3d_array(ls, start, stop, m, n, n_frames_in_video):
+    if stop <= n_frames_in_video -1:
+        l = stop
+    else:
+        l = n_frames_in_video - 1
+    frames_array = np.zeros((m, n, l))
+    for i in range(start, stop):
+        frame = cv2.imread(str(ls[i]), cv2.IMREAD_GRAYSCALE)
+        frames_array[:, :, i] = frame
+    # for i, frame_img in enumerate(ls):
+    #     frame = cv2.imread(str(frame_img), cv2.IMREAD_GRAYSCALE)
+    #     frames_array[:, :, i] = frame
+    return frames_array, l
+
 
 
